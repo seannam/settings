9939afe5a9eacff26d58a3bcf9be203d2ff04bef
Sean Nam <namsangwoo1@gmail.com>
2018-05-11 23:53:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Settings
+Backing up custom alias and other bash profile settings, vimrc, and more because setting up new systems suck.
---
+++ my_functions.sh
@@ -0,0 +1,13 @@
+# https://apple.stackexchange.com/questions/242190/moving-bash-profile-to-dropbox/242191
+
+
+alias gith='cd ~/GitWorkspace/'
+alias jflap='java -jar ~/Documents/JFLAP.jar'
+
+# Setting PATH for Python 3.6
+# The original version is saved in .bash_profile.pysave
+# PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
+# PATH=$HOME/Library/bin:$PATH
+# export PATH
+
+# export PYTHONPATH="/Library/Frameworks/Python.framework/Versions/3.6/Python"
