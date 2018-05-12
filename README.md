83926a7e5598e7258bfaab823ae53c2ad88dc32e
Sean Nam <namsangwoo1@gmail.com>
2018-05-12 00:09:39 -0700

    Moved link from sh to md file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Settings
 Backing up custom alias and other bash profile settings, vimrc, and more because setting up new systems suck.
+
+### Resources
+https://apple.stackexchange.com/questions/242190/moving-bash-profile-to-dropbox/242191
--- my_functions.sh
+++ my_functions.sh
@@ -1,8 +1,5 @@
-# https://apple.stackexchange.com/questions/242190/moving-bash-profile-to-dropbox/242191
-
-
 alias gith='cd ~/GitWorkspace/'
-alias jflap='java -jar ~/Documents/JFLAP.jar'
+# alias jflap='java -jar ~/Documents/JFLAP.jar'
 
 # Setting PATH for Python 3.6
 # The original version is saved in .bash_profile.pysave
